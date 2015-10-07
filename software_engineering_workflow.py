2bc9afb10d5df3daf8482111c9f4c2793c538547
Tom Wallis <tom@tom-wallis.com>
2015-10-07 21:37:00 +0100

    Corrected flow, and now prints trace of actions

--- software_engineering_workflow.py
+++ software_engineering_workflow.py
@@ -19,60 +19,89 @@ def random_boolean():
     return random_float() > 0.5
 
 
-
 # -----------------------------------------------------------------------------
 
 
+def run_model():
+    create_branch()
+
 # The functions that act as actions within the flowchart
 def create_branch():
-    pass
+    print "Creating branch"
+    write_tests()
 
 def write_tests():
+    print "Writing tests"
     resources['money'] -= 100
     resources["stress"] += 0.1
+    write_code()
 
 def write_code():
+    print "Writing code"
     resources["money"] -= 100
     resources["stress"] += 0.05
+    run_tests()
 
 def run_tests():
+    print "Running tests against code"
     resources["building"] = random_boolean()  
     resources["money"] -= 100
     if resources["building"]:
+        print "\t- Tests successful!"
         resources["stress"] -= 0.1
+        integration_test()
     else:
+        print "\t- Tests unsuccessful"
+        print "\t- Rewriting tests"
         resources["stress"] += 0.1
         write_tests()
 
-
 def integration_test():
+    print "Running integration test"
     resources["money"] -= 100
     resources["building"] = random_boolean()  
     if resources["building"]:
+        print "\t- Integration test successful!"
         resources["stress"] -= 0.05
+        merge()
     else:
+        print "\t- Integration test unsuccessful"
+        print "\t- Rewriting tests"
         resources["stress"] += 0.05
         write_tests()
 
 def merge():
+    print "Merging branch against codebase"
     resources["money"] -= 100
     resources["stress"] += 0.05  # Because merging is always stressful, particularly with a large codebase. 
     # Perhaps this should scale to some "changes made" resource that gets incremented by the code and test writing?
+    user_acceptance_test()
 
 def user_acceptance_test():
+    print "Performing user acceptance tests"
     resources["money"] -= 100
     resources["building"] = random_boolean()  
     if resources["building"]:
+        print "\t- Test sucessful!"
         resources["stress"] -= 0.05
+        deploy()
     else:
+        print "\t- Test unsuccessful"
+        print "\t- Rewriting tests"
         resources["stress"] += 0.05
         write_tests()
 
 def deploy():
+    print "Deploying!"
     resources["stress"] += 0.1
     resources["money"] -= 100
     if not resources["building"]:
+        print "\t- Problems with building"
+        print "\t- Rerunning integration test"
         integration_test()
+    else:
+        print "\t- Deployment successful!"
+        complete()
 
 def complete():
     print "--------------------------------------------------------------------------------"
@@ -84,13 +113,4 @@ def complete():
 
 
 # -----------------------------------------------------------------------------
-
-
-create_branch()
-write_tests()
-run_tests()
-integration_test()
-merge()
-user_acceptance_test()
-deploy()
-complete()
+run_model()
