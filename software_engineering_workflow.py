467ca6710a2ced77a0ed585e9fef9fd1d8a02555
Tom Wallis <tom@tom-wallis.com>
2015-10-12 17:33:13 +0100

    Began using logger rather than printing

--- software_engineering_workflow.py
+++ software_engineering_workflow.py
@@ -1,4 +1,5 @@
-from random import random as random_float
+import random
+from logger import Logger
 
 # Resources regarding the workflow's environment
 time = 0 # to be incremented upon every step
@@ -10,13 +11,13 @@ resources = {}
 resources["stress"] = 0.1
 resources["building"] = True
 resources["money"] = 10000
-
+log = Logger()
 
 # -----------------------------------------------------------------------------
 
 
 def random_boolean():
-    return random_float() > 0.5
+    return random.choice([True, False])
 
 
 # -----------------------------------------------------------------------------
@@ -27,88 +28,90 @@ def run_model():
 
 # The functions that act as actions within the flowchart
 def create_branch():
-    # print "Creating branch"
+    log.log_line("Creating new branch")
     # write_tests()
 
 def write_tests():
-    # print "Writing tests"
+    log.log_line("Writing tests")
     resources['money'] -= 100
     resources["stress"] += 0.1
     # write_code()
 
 def write_code():
-    # print "Writing code"
+    log.log_line("Writing code")
     resources["money"] -= 100
     resources["stress"] += 0.05
     # run_tests()
 
 def run_tests():
-    # print "Running tests against code"
+    log.log_line("Running tests against code")
     resources["building"] = random_boolean()  
     resources["money"] -= 100
     if resources["building"]:
-        # print "\t- Tests successful!"
+        log.log_line("\t- Tests successful!")
         resources["stress"] -= 0.1
         # integration_test()
     else:
-        # print "\t- Tests unsuccessful"
-        # print "\t- Rewriting tests"
+        log.log_line("\t- Tests unsuccessful")
+        log.log_line("\t- Rewriting tests")
         resources["stress"] += 0.1
         # write_tests()
 
 def integration_test():
-    # print "Running integration test"
+    log.log_line("Running integration test")
     resources["money"] -= 100
     resources["building"] = random_boolean()  
     if resources["building"]:
-        # print "\t- Integration test successful!"
+        log.log_line("\t- Integration test successful!")
         resources["stress"] -= 0.05
         # merge()
     else:
-        # print "\t- Integration test unsuccessful"
-        # print "\t- Rewriting tests"
+        log.log_line("\t- Integration test unsuccessful")
+        log.log_line("\t- Rewriting tests")
         resources["stress"] += 0.05
         # write_tests()
 
 def merge():
-    # print "Merging branch against codebase"
+    log.log_line("Merging branch against codebase")
     resources["money"] -= 100
     resources["stress"] += 0.05  # Because merging is always stressful, particularly with a large codebase. 
     # Perhaps this should scale to some "changes made" resource that gets incremented by the code and test writing?
     # user_acceptance_test()
 
 def user_acceptance_test():
-    # print "Performing user acceptance tests"
+    log.log_line("Performing user acceptance tests")
     resources["money"] -= 100
     resources["building"] = random_boolean()  
     if resources["building"]:
-        # print "\t- Test sucessful!"
+        log.log_line("\t- Test successful!")
         resources["stress"] -= 0.05
         # deploy()
     else:
-        # print "\t- Test unsuccessful"
-        # print "\t- Rewriting tests"
+        log.log_line("\t- Test unsuccessful")
+        log.log_line("\t- Rewriting tests")
         resources["stress"] += 0.05
         # write_tests()
 
 def deploy():
-    # print "Deploying!"
+    log.log_line("Deploying product")
     resources["stress"] += 0.1
     resources["money"] -= 100
     if not resources["building"]:
-        # print "\t- Problems with building"
-        # print "\t- Rerunning integration test"
+        log.log_line("\t- Problems with building")
+        log.log_line("\t- Rerunning integration test")
         integration_test()
     else:
-        # print "\t- Deployment successful!"
+        log.log_line("\t- Deployment successful!")
         # complete()
 
 def complete():
-    print "--------------------------------------------------------------------------------"
+    print
     print "Complete!"
+    log.log_line("--------------------------------------------------------------------------------")
+    log.log_line("Complete!")
 
     for key in resources.keys():
-        print key + ":\t\t" + str(resources[key])
+        log.log_line(key + ":\t\t" + str(resources[key]))
     print "--------------------------------------------------------------------------------"
 
 
