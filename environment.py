bd7d341cf7d52f3a4051c8f0695cf6a2d3688521
Tom Wallis <tom@tom-wallis.com>
2015-10-29 15:30:04 +0000

    Fixed broken resources

--- environment.py
+++ environment.py
@@ -1,5 +1,5 @@
 # The resources we'll be editing
-# NOTE: These should ordinarily be amended to reflect the project...
+# NOTE: These should ordinarily be amended to reflect the initial state of the project...
 resources = {}
 resources["stress"] = 0.1
 resources["building"] = True
@@ -8,3 +8,5 @@ resources["tests passing"] = False
 resources["integration tests passing"] = False
 resources["user acceptance tests passing"] = False
 resources["successful deployment"] = False
+resources["tickets"] = 0
+resources["branches"] = 1
