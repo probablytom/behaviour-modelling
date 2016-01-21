ac2c79ce50c63309747e3f9be00d7a53aa069797
Tom Wallis <github@tomwallis.net>
2016-01-21 20:37:05 +0000

    Beginnings of caching and almost-working dynamic mutation

--- mutators/base.py
+++ mutators/base.py
@@ -1,119 +1,59 @@
-import sys, re, ast, random
+import sys, re, ast, random, ast, codegen, inspect, base, copy
 from codegen import to_source
-
-
-INDENT_PAT = re.compile('^([ \t]*)[^ \t]', re.MULTILINE)
 
 class MutantTransformer(ast.NodeTransformer):
 
-    def __init__(self, source_lines, mutation='comment_line', strip_decorators = True):
+    def __init__(self, mutation='comment_single_line', strip_decorators = True):
         self.source_lines = source_lines
         self.strip_decorators = strip_decorators
         self.mutation = mutation
 
-    def comment_line(self, line_node, lineno = None, indent = None):
-        if lineno == None: lineno = line_node.lineno
-        line = self.source_lines[lineno-1]
-        if indent is None:
-            indent = self.indentation(line)
-        self.source_lines[lineno-1] = (indent + '#Mutant#  ' + line[len(indent):]).rstrip() + '\n'
-        return indent
-
-    # Returns line number comented or -1 if commenting failed
-    def attempt_comment_from(self, lineno, indent = None):
-        can_comment = False
-        will_comment = True
-        lineno -= 1
-        
-        while not can_comment:
-            lineno += 1
-            can_comment, will_comment = self.commentable_line(self.source_lines[lineno])
-            if not will_comment:
-                
-                return -1  # Couldn't comment
-        line = self.source_lines[lineno-1]
-        if indent is None:
-            indent = self.indentation(line)
-        self.source_lines[lineno-1] = (indent + '#Mutant#  ' + line[len(indent):]).rstrip() + '\n'
-        return 0
-
-    def attempt_comment_node(self, node):
-        linecount = 0
-        for line in node.body:
-            if not ('body' in dir(line)):
-                line_source = self.source_lines[line.lineno-1]
-                indent = self.indentation(line_source)
-                self.source_lines[line.lineno-1] = (indent + '#Mutant#  ' + line_source[len(indent):]).rstrip() + '\n'
-                return linecount
-            linecount += 1
-        return -1
-
-    def indentation(self, line):
-        m = INDENT_PAT.search(line)
-        if m:
-            return m.group(1)
-        return ''
-
-    # Currently doesn't deal with nested functions correctly, see 'def'
-    def commentable_line(self, line):
-        can_comment = True
-        will_comment = True
-        if "def" in line:
-            can_comment = False
-        if "for" in line:
-            can_comment = False
-            will_comment = False
-        if "if" in line:
-            can_comment = False
-            will_comment = False
-        if "while" in line:
-            can_comment = False
-            will_comment = False
-        if "else" in line:
-            can_comment = False
-            will_comment = False
-        if "class" in line:
-            can_comment = False
-            will_comment = False
-        return can_comment, will_comment
-
-    def prepare_mutest_import_node(self):
-        return ast.ImportFrom(module = 'mutester.public', names = [ ast.alias(name = 'mutest_statement_reached', asname = None) ], level = 0)
-
-    def prepare_mutest_reached_node(self):
-        return ast.Call(func = ast.Name(id = 'mutest_statement_reached'), args = [ ], keywords = [ ], starargs = None, kwargs = None)
-
-    def print_source_lines(self):
-        for lineno, line in enumerate(self.source_lines):
-            sys.stdout.write('%03d: %s' % ( lineno+1, line ))
-
     #NOTE: This will work differently depending on whether the decorator takes arguments. 
     def visit_FunctionDef(self, node):
-        mutatables = []
         if self.strip_decorators == True:
             node.decorator_list = []
-        linesToCheck = [node.body]
-# This commented out because I suspect this actually doesn't make us any more secure.
-#         for lineSet in linesToCheck:
-#             for item in lineSet:
-#                 if not hasattr(item, 'body'):
-#                     mutatables.append(item)
-#                 else:
-#                     pass#lineSet.append(item.body)
-        if self.mutation == 'comment_line':
-            if len(mutatables) > 0: node.body.remove(random.choice(mutatables))
+        lines_to_check = node.body
+        if self.mutation == 'comment_single_line':
+            if len(lines_to_check) > 0: node.body.remove(random.choice(lines_to_check))
         elif self.mutation == 'truncate_function':
-            truncation_point = random.randint(0,len(mutatables))
-            node.body = node.body[0:truncation_point]
+            truncation_point = random.randint(0,len(lines_to_check) - 1)
+            node.body = node.body[truncation_point:]
         return self.generic_visit(node)
 
-if __name__ == "__main__":
-    with open("test_code_to_mutate.py") as env:
-        uin = env.read()
-        visitor = MutantTransformer(uin.split('\n'))
-        t = ast.parse(uin)
-        t =visitor.visit(t)
-        print to_source(t)
-        print
-        print
-        exec( to_source(t) )#exec('\n'.join(visitor.source_lines))
+def mutate__comment_single_line(func):
+    source_to_run = ""
+    def wrapper(*args, **kwargs):
+        to_run = generic_retrieve_mutation(func, 'comment_single_line')
+        to_run(*args, **kwargs)
+    return wrapper
+
+def mutate__truncate_function(func):
+    source_to_run = ""
+    def wrapper(*args, **kwargs):
+        generic_retrieve_mutation(func, 'truncate_function')(*args, **kwargs)
+        print cache
+    return wrapper
+
+def generic_retrieve_mutation(func, mutation_type):
+    # Commented so we don't really cache
+    # if (func, mutation_type) in cache.keys(): 
+    #    return cache[(func, mutation_type)]
+    compiled_mutant = mutate(func, mutation_type)
+    mutated_function = copy.copy(func)
+    mutated_function.func_code = compiled_mutant
+    cache[func] = mutated_function
+    return mutated_function
+
+def mutate(function, mutation_type = "comment_single_line"):
+    function_source = inspect.getsourcelines(function)[0]
+    function_source = ''.join(function_source) + '\n' + function.func_name + '()'
+    mutator = MutantTransformer(function_source, mutation_type)
+    abstract_syntax_tree = ast.parse(function_source)
+    mutated_function = mutator.visit(abstract_syntax_tree)
+    return compile(mutated_function, '<ast>', 'exec')
+
+def exec_wrapper(mutant):
+    exec(mutant)
+
+# random.seed("mutation testing")
+cache = {}
--- mutators/decorator_runtime_test.py
+++ mutators/decorator_runtime_test.py
@@ -1,34 +1,5 @@
-import ast, codegen, inspect, base
-def mutate__comment_single_line(func):
-    source_to_run = ""
-    def wrapper(*args, **kwargs):
-        original_source =inspect.getsourcelines(func)[0] 
-        func_source = ''.join(original_source) + '\n' + func.func_name + '()'
-        t = ast.parse(func_source)
-        mutator = base.MutantTransformer(func_source, 'comment_line')
-        mutated_func = mutator.visit(t)
-        compiled_mutant = compile(mutated_func, '<ast>', 'exec')
-        exec_wrapper(compiled_mutant)
-        #func.func_code = compiled_mutant
-        #func(*args, **kwargs)
-    return wrapper
-
-def mutate__truncate_function(func):
-    source_to_run = ""
-    def wrapper(*args, **kwargs):
-        original_source =inspect.getsourcelines(func)[0] 
-        func_source = ''.join(original_source) + '\n' + func.func_name + '()'
-        t = ast.parse(func_source)
-        mutator = base.MutantTransformer(func_source, 'truncate_function')
-        mutated_func = mutator.visit(t)
-        compiled_mutant = compile(mutated_func, '<ast>', 'exec')
-        exec_wrapper(compiled_mutant)
-        #func.func_code = compiled_mutant
-        #func(*args, **kwargs)
-    return wrapper
-
-def exec_wrapper(mutant):
-    exec(mutant)
+from base import *
+import inspect
 
 @mutate__comment_single_line
 def test_comment():
@@ -53,3 +24,18 @@ def test_truncate():
 test_comment()
 print
 test_truncate()
+print
+test_comment()
+print
+test_truncate()
+print
+test_truncate()
+print
+test_truncate()
+print
+test_truncate()
+print
+test_truncate()
+print
+print
+print inspect.getsourcelines(test_truncate)
