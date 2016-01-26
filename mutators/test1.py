4dd3fc67342912130fbfdfaf8783d807d2b2c6ad
Tom Wallis <github@tomwallis.net>
2016-01-26 17:43:40 +0000

    Deleting old files, tidying up a touch

--- mutators/test1.py
+++
@@ -1,7 +0,0 @@
-import base
-if __name__ == "__main__":
-    print base.cache
-    import test2
-else:
-    import base
-    print base.cache
--- mutators/test2.py
+++
@@ -1,5 +0,0 @@
-from base import *
-cache.append('test2')
-print cache
-import test1
-
