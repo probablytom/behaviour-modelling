57718aeccbe2700732e5959d7eefb37bbaa2e457
Tom Wallis <tom@tom-wallis.com>
2016-02-01 12:25:41 +0000

    Another unittest for randomness testing...

--- unittests.py
+++ unittests.py
@@ -15,5 +15,30 @@ class TestMutationRandomness(unittest.TestCase):
         print decorator_runtime_test.results
         self.assertNotEqual(decorator_runtime_test.results[1], decorator_runtime_test.results[2])
 
+    def test_mutation_randomness(self):
+        mutation_randomness_test0()
+        mutation_randomness_test1()
+        self.assertNotEqual(results[0], results[1])
+        
+@mutate__comment_single_line
+def mutation_randomness_test0():
+    results[0] = 0
+    results[0] += 1
+    results[0] += 2
+    results[0] += 3
+    results[0] += 4
+    results[0] += 5
+
+@mutate__comment_single_line
+def mutation_randomness_test_result1():
+    results[1] = 0
+    results[1] += 1
+    results[1] += 2
+    results[1] += 3
+    results[1] += 4
+    results[1] += 5
+
+results = [0, 0, 0]
+
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
